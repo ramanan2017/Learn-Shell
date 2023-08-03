0a829dede4a6f94c6948e7dfc8c522f89e1bcfc2
Ram <ratram2010@gmail.com>
2023-08-03 23:04:29 +0100

    Function updated

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,3 +3,13 @@ a=10
 #Both are same
 echo a is $a
 echo a is ${a}
+
+#Substituion Variable
+## Command Substitution
+DATE=$(date)
+
+echo Today Date is $DATE
+
+## Arithmetic Substitution
+ADD=$(( 2+2 ))
+echo ADD of 2+2 = $ADD
--- 04-function.sh
+++ 04-function.sh
@@ -1,7 +1,21 @@
 greeting(){
   echo Hello, Good Morning Ram
   echo Welcome to DevOps Training
+  return 10       # will return 10
   echo Good to Have you hear
 }
 
 greeting
+echo Function Exit Status - $?
+
+
+# Function have its own special variable
+
+input() {
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments- $*
+  echo No of Arguments - $#
+}
+
+input abc 1234
